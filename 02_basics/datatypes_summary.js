08fc103ca01fb211efdfaf3d762c664877f9e2a7
Anupam Dubey <anupamdubey@Anupams-MacBook-Air.local>
2025-11-27 17:53:17 +0530

    initial commit

---
+++ 02_basics/datatypes_summary.js
@@ -0,0 +1,15 @@
+console.log("Anupam Dubey")
+
+const nu = 234597762848
+console.log(typeof nu)
+
+let userOne ={
+    email: "anupam@gmail.com",
+    upi: "ad@oksbi" 
+}
+
+let userTwo = userOne 
+    userTwo.email = "anupam1606@gmail.com"
+
+    console.log(userTwo.email)
+    console.log(userOne.email) 
