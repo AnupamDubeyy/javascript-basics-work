104a38d47887121117a0ca9f010dafa61856c929
Anupam Dubey <anupamdubey@Anupams-MacBook-Air.local>
2025-11-27 23:17:51 +0530

    day2

---
+++ 03_basics/string.js
@@ -0,0 +1,29 @@
+var name = "Anupam" 
+var repoCount = 50 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) 
+
+// To define String there are two ways by which we can define them
+
+var string1 = "Hello World"
+console.log(string1)
+
+//second way 
+
+const game_name = new String("VolleyBall")
+console.log(game_name[2])
+console.log(game_name.length)
+
+const anotherStr = game_name.slice(0,6)
+console.log(anotherStr) 
+
+// let name = "    anupam      "
+// console.log(name.trim())
+// console.log(name)
+
+const url = "https://www.youtube.com"
+console.log(url.replace("https","http"))
+
+console.log(url.includes("youtube"))
+
+console.log(game_name)
