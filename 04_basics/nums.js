c81a9ea53c7e165f4efe3b484452dca853c0f1fb
Anupam Dubey <anupamdubey@Anupams-MacBook-Air.local>
2025-11-30 00:22:33 +0530

    nums_in_js

---
+++ 04_basics/nums.js
@@ -0,0 +1,14 @@
+const score = 400 
+console.log(score)
+
+const balance = new Number(100)          //creates a Number object
+console.log(balance)
+console.log(balance.toString().length)   // .length shows number of characters
+console.log(balance.toFixed(2))         //used for formatting currency
+
+const otherNumber = 122.342 
+console.log(otherNumber.toPrecision(4))  //limits total number of digits 
+
+const hundreds = 1000000 
+console.log(hundreds.toLocaleString('en-IN')) //formats number based on locale
+
